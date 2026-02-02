b1e308e87fe647ee5bf2133fde1c40568f08e46a
eyrebuild <nothanks@fake.com>
2026-02-02 20:57:51 +0000

    Project set up

---
+++ src/scripts/screenshot.js
@@ -0,0 +1,19 @@
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch({
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu',
+  ],
+});
+const page = await browser.newPage();
+await page.goto('https://eyrebuild.github.io/honeymoon-planner/', {
+  waitUntil: 'networkidle2',
+});
+await page.screenshot({
+  path: 'public/homepage-screenshot.png',
+});
+
+await browser.close();
